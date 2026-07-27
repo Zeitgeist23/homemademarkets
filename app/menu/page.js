69373ce36020c21a251417310fefb2e3a6f79933
Zeitgeist23 <JWigg023@gmail.com>
2026-07-27 14:55:53 -0500

    Load menu 3D preview styles

--- app/menu/page.js
+++ app/menu/page.js
@@ -2,6 +2,7 @@ import FunctionalMenu from '../../components/FunctionalMenu';
 import './menu.css';
 import './menu-hotspot-fix.css';
 import './menu-bottom-spacing.css';
+import './menu-3d.css';
 
 export const metadata = {
   title: 'Menu | Homemade Markets',
