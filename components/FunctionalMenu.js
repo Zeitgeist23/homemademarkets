972475c6d3c1af6ef54df661080ac94b153dc680
Zeitgeist23 <JWigg023@gmail.com>
2026-07-27 09:53:53 -0500

    Remove See Our Menu hotspot from menu page

--- components/FunctionalMenu.js
+++ components/FunctionalMenu.js
@@ -156,7 +156,6 @@ export default function FunctionalMenu() {
         <a className="poster-hotspot poster-catering" href="/#catering" aria-label="Catering information" />
         <button className="poster-hotspot poster-shop" type="button" onClick={() => setDrawerOpen(true)} aria-label={`Open order cart with ${itemCount} items`} />
         <a className="poster-hotspot poster-logo-home" href="/" aria-label="Return to the Homemade Markets landing page" />
-        <a className="poster-hotspot poster-see-menu" href="#menu-top" aria-label="See the Homemade Markets menu" />
         <a className="poster-hotspot poster-find-market" href="/#market-dates" aria-label="Find Homemade Markets at a market" />
         <button
           className="poster-hotspot poster-place-order"
